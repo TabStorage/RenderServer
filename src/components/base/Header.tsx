6dbfb433f31e88220b6ae030dfd9c194b795ac2f
kimeuichan <eck7965@gmail.com>
2019-05-31 17:37:57 +0900

    update schema

---
+++ src/components/base/Header.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export const Header: React.FC = props => {
+    return (
+        <div>
+            <header className="header">
+                <div className="toggle-button">
+
+                </div>
+                <div className="icon">
+                    
+                </div>
+                <div className="my-page">
+                    
+                </div>
+            </header>
+        </div>
+    );
+};
